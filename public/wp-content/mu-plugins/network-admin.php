7fecedbc0861d21c91740c23f3459361fcaf5247
Gemma Black <gblackuk@googlemail.com>
2018-05-07 21:51:03 +0100

    Composer name changed to generic WordPress Composer Starter
    As this could be used as a starter for both single WordPress projects and also the WordPress multisite network

--- public/wp-content/mu-plugins/network-admin.php
+++ public/wp-content/mu-plugins/network-admin.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Redirects the url to the subfolder that the WordPress core has been moved to
+ */
 add_filter('network_admin_url', function($url, $path) {
     $networkPath = str_replace( '/wp-admin/', WP_ADMIN_DIR . '/wp-admin/', $url );
     return $networkPath;
